d2338738617df51578dc94f23c2cdf3d30bfbf4c
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-09-23 23:33:52 +0530

    modify nav bar
    Signed-off-by: Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,25 +2,25 @@
     <div>
         <ul class="nav ">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link active fs-4" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/about">About Us</a>
+                <a class="nav-link fs-4" href="/about">About Us</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact Us</a>
+                <a class="nav-link fs-4" href="/contact">Contact Us</a>
             </li>
 
         </ul>
     </div>
 
     <div>
-        <ul class="nav ">
+        <ul class="nav gap-3">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/login">Login</a>
+                <a class="fs-5 btn btn-success" aria-current="page" href="/login">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/register">Register</a>
+                <a class="fs-5 btn btn-warning" href="/register">Register</a>
             </li>
         </ul>
     </div>
