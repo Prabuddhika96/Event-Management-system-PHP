f42307daa6e91daad1bacc03940a1fb7dfa815ef
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-09-23 22:35:44 +0530

    add about page
    Signed-off-by: Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('title')
+    About Us
+@endsection
+
+@section('content')
+    <h1>ABOUT US</h1>
+
+    <p>this is about us page</p>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>@yield('title', 'Learnig Laravel')</title>
+    <title>@yield('title', 'Event Management System')</title>
     {{-- 2nd one is default one --}}
 
     {{-- boostrap --}}
