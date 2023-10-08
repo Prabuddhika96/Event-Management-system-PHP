fcde2d67ebd927734ea66e1d8dd4563f70ac1e55
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-10-08 21:29:49 +0530

    change home banner
    Signed-off-by: Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,14 +1,14 @@
-<section class="banner-two bg-banner-two overlay-white-slant text-overlay">
-    <div class="container">
+<section class="banner-two "
+    style="background: url('{{ asset('storage/' . $event->photo_path) }}') no-repeat; background-size: cover; background-position: right center;">
+    <div class="container" style="background-color: rgba(240, 248, 255, 0.688); text-align: center; padding: 20px 10px">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <!-- Content Block -->
                 <div class="block">
                     <h1>{{ $event->event_name }}</h1>
                     <h3>2023</h3>
                     <h4>{{ $event->date }}</h4>
                     <h4>{{ $event->time }}</h4>
-
                 </div>
             </div>
         </div>
